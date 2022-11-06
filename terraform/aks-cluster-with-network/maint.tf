11876aa9cefcb8a71f67d8b6cc6f4da6aea2dd61
mnicolescu <mnicolescu@gmail.com>
2022-11-06 11:03:41 +0100

    Add prevent_deletion_if_contains_resources to false for azure rm resource_group feature

--- terraform/aks-cluster-with-network/maint.tf
+++ terraform/aks-cluster-with-network/maint.tf
@@ -31,7 +31,11 @@ terraform {
 provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 

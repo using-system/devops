8f76de2683a719f8c92c427c5e25c8da6eb92652
mnicolescu <mnicolescu@gmail.com>
2023-05-11 21:02:13 +0200

    Integrate terrascan in tf plan github workflow

--- terraform/modules/az-building-blocks/rg/main.tf
+++ terraform/modules/az-building-blocks/rg/main.tf
@@ -3,13 +3,3 @@ resource "azurerm_resource_group" "rg" {
   location                  =  var.location
   tags                      =  var.tags
 }
-
-resource "azurerm_management_lock" "rg_lock" {
-
-  depends_on = [ azurerm_resource_group.rg ]
-
-  name       = "${azurerm_resource_group.rg.name}-lock"
-  scope      = azurerm_resource_group.rg.id
-  lock_level = "CanNotDelete"
-  
-}

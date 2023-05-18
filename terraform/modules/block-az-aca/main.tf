5c36a861a3f89d41e6d53bc49ba293b320435fa4
mnicolescu <mnicolescu@gmail.com>
2023-05-18 20:47:26 +0200

    Add az-aca tf module

---
+++ terraform/modules/block-az-aca/main.tf
@@ -0,0 +1,41 @@
+resource "azurerm_container_app" "aca" {
+  name                         = var.name
+  container_app_environment_id = var.container_app_environment_id
+  resource_group_name          = var.resource_group_name
+  revision_mode                = var.revision_mode
+
+  template {
+    min_replicas = var.min_replicas
+    max_replicas = var.max_replicas
+    container {
+      name   = "containerapp"
+      image  = var.image
+      cpu    = var.cpu
+      memory = var.memory
+
+      dynamic "environment_variable" {
+        for_each = var.environment_variables
+        content {
+          name        = environment_variable.value.name
+          value       = environment_variable.value.value
+          secret_name = environment_variable.value.secret_name
+        }
+      }
+
+      identity {
+        type         = var.identity_type
+        identity_ids = var.identity_ids
+      }
+
+      dynamic "ingress" {
+        count = var.enable_ingress ? 1 : 0
+        content {
+          allow_insecure_connections = var.allow_insecure_connections
+          external_enabled           = var.external_enabled
+          target_port                = var.target_port
+        }
+      }
+
+    }
+  }
+}

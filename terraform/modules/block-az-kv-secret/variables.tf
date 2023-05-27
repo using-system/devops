a9ac2624e4a801abb1fc8317d469fbfb6e13b793
mnicolescu <mnicolescu@gmail.com>
2023-05-27 16:36:31 +0200

    Add az-kv-secret tf module

---
+++ terraform/modules/block-az-kv-secret/variables.tf
@@ -0,0 +1,14 @@
+variable "kv_id" {
+  description = "The key vault id"
+  type        = string
+}
+
+variable "secret_name" {
+  description = "The secret name"
+  type        = string
+}
+
+variable "secret_value" {
+  description = "The secret value"
+  type        = string
+}

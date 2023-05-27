23d813443ec7df103dfd49a92a1dc60dff229396
mnicolescu <mnicolescu@gmail.com>
2023-05-27 23:50:44 +0200

    Add az-dns-zone-vnet-link tf module

---
+++ terraform/modules/block-az-dns-zone-vnet-link/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_private_dns_zone_virtual_network_link" "kv" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+
+  private_dns_zone_name = var.private_dns_zone_name
+  virtual_network_id    = var.vnet_id
+  registration_enabled  = var.registration_enabled
+
+  tags = var.tags
+}

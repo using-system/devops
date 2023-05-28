998db53e85bc3133267379dfc51ebb11ed6f9225
mnicolescu <mnicolescu@gmail.com>
2023-05-28 14:51:46 +0200

    Add enabled variable for bastion tf module

--- terraform/modules/block-az-bastion/main.tf
+++ terraform/modules/block-az-bastion/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_bastion_host" "bastion" {
   ip_configuration {
     name                 = "configuration"
     subnet_id            = var.subnet_id
-    public_ip_address_id = azurerm_public_ip.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion[0].id
   }
 
   sku                = var.sku

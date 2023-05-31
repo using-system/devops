35f37a110b114ae05677730289cdcec417d83aaa
mnicolescu <mnicolescu@gmail.com>
2023-05-31 22:35:31 +0200

    Add az aks module

--- terraform/modules/block-az-aks/outputs.tf
+++ terraform/modules/block-az-aks/outputs.tf
@@ -48,10 +48,10 @@ output "kube_config_raw" {
 output "generated_cluster_private_ssh_key" {
   description = "The cluster will use this generated private key as ssh key when `var.public_ssh_key` is empty or null. Private key data in [PEM (RFC 1421)](https://datatracker.ietf.org/doc/html/rfc1421) format."
   sensitive   = true
-  value       = try(azurerm_kubernetes_cluster.aks.linux_profile[0], null) != null ? (var.configuration.public_ssh_key == "" || var.configuration.public_ssh_key == null ? tls_private_key.ssh[0].private_key_pem : null) : null
+  value       = try(azurerm_kubernetes_cluster.aks.linux_profile[0], null) != null ? (var.configuration.public_ssh_key == "" || var.configuration.public_ssh_key == null ? tls_private_key.aks[0].private_key_pem : null) : null
 }
 
 output "generated_cluster_public_ssh_key" {
   description = "The cluster will use this generated public key as ssh key when `var.public_ssh_key` is empty or null. The fingerprint of the public key data in OpenSSH MD5 hash format, e.g. `aa:bb:cc:....` Only available if the selected private key format is compatible, similarly to `public_key_openssh` and the [ECDSA P224 limitations](https://registry.terraform.io/providers/hashicorp/tls/latest/docs#limitations)."
-  value       = try(azurerm_kubernetes_cluster.aks.linux_profile[0], null) != null ? (var.configuration.public_ssh_key == "" || var.configuration.public_ssh_key == null ? tls_private_key.ssh[0].public_key_openssh : null) : null
+  value       = try(azurerm_kubernetes_cluster.aks.linux_profile[0], null) != null ? (var.configuration.public_ssh_key == "" || var.configuration.public_ssh_key == null ? tls_private_key.aks[0].public_key_openssh : null) : null
 }

213e9b587ebc3fdfe440f42f231439c3bb42bbeb
mnicolescu <mnicolescu@gmail.com>
2023-06-10 19:09:28 +0200

    Add name ouput for az-rg tf mofule

--- terraform/modules/block-az-rg/outputs.tf
+++ terraform/modules/block-az-rg/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "Resource group id"
   value       = azurerm_resource_group.rg.id
 }
+
+output "name" {
+  description = "Resource group name"
+  value       = var.name
+}

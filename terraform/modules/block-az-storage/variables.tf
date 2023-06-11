6862660df2983320e3e917aa3603ba11fd0cb1bf
mnicolescu <mnicolescu@gmail.com>
2023-06-11 15:45:49 +0200

    Add az-storage tf module

--- terraform/modules/block-az-storage/variables.tf
+++ terraform/modules/block-az-storage/variables.tf
@@ -3,6 +3,11 @@ variable "resource_group_name" {
   type        = string
 }
 
+variable "name" {
+  description = "Specifies the name of the storage account."
+  type        = string
+}
+
 variable "location" {
   description = "The location of the resource group in which to create the storage account."
   type        = string

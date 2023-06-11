4ecd2bded4d1869d0c674ab7d9d4f788d387e98c
mnicolescu <mnicolescu@gmail.com>
2023-06-11 18:24:23 +0200

    Add output for az-identity tf module

--- terraform/modules/block-az-assigned-identity/ouputs.tf
+++ terraform/modules/block-az-assigned-identity/ouputs.tf
@@ -1,3 +1,15 @@
 output "id" {
   value = azurerm_user_assigned_identity.identity.id
 }
+
+output "principal_id" {
+  value = azurerm_user_assigned_identity.identity.principal_id
+}
+
+output "client_id" {
+  value = azurerm_user_assigned_identity.identity.client_id
+}
+
+output "tenant_id" {
+  value = azurerm_user_assigned_identity.identity.tenant_id
+}

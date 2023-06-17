699645b5b19180b6e8381b2b0518b04d677c52c4
mnicolescu <mnicolescu@gmail.com>
2023-06-17 17:34:36 +0200

    Update kv-cust-key tf module

--- terraform/modules/block-az-storage/maint.tf
+++ terraform/modules/block-az-storage/maint.tf
@@ -41,7 +41,7 @@ resource "azurerm_storage_account" "storage" {
   tags = var.tags
 
   lifecycle {
-    ignore_changes = [customer_managed_key]
+    ignore_changes = [customer_managed_key, network_rules.private_link_access]
   }
 }
 

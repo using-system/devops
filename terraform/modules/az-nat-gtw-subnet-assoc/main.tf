1d1582a0281ecaea736623372121101b1db111f3
Matthieu Nicolescu <mnicolescu@gmail.com>
2023-08-26 19:41:00 +0200

    Add az-nat-gtw module

--- terraform/modules/az-nat-gtw-subnet-assoc/main.tf
+++ terraform/modules/az-nat-gtw-subnet-assoc/main.tf
@@ -3,5 +3,5 @@ resource "azurerm_subnet_nat_gateway_association" "sn_cluster_nat_gw" {
   for_each = toset(var.subnet_ids)
 
   subnet_id      = each.key
-  nat_gateway_id = vat.nat_gateway_id
+  nat_gateway_id = var.nat_gateway_id
 }

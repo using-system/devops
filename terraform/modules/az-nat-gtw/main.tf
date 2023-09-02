82aa05ebd60847ebff651caf52038526e1b457ad
Matthieu Nicolescu <mnicolescu@gmail.com>
2023-09-02 18:12:58 +0200

    Add az-nat-gtw module

--- terraform/modules/az-nat-gtw/main.tf
+++ terraform/modules/az-nat-gtw/main.tf
@@ -2,18 +2,16 @@ locals {
   ips_with_index = { for idx, ip in var.public_ips : idx => ip }
 }
 
-resource "azurerm_public_ip_prefix" "nat_prefix" {
+resource "azurerm_public_ip" "nat_gtw" {
+
   for_each = local.ips_with_index
 
-  name                = "pipp-${var.name}-${each.key}"
-  resource_group_name = var.resource_group_name
+  name                = "pip-${var.name}-${each.key}"
   location            = var.location
+  resource_group_name = var.resource_group_name
   ip_version          = each.value.ip_version
-  prefix_length       = each.value.prefix_length
+  allocation_method   = "Dynamic"
   sku                 = each.value.sku
-  zones               = each.value.availability_zones
-
-  tags = var.tags
 }
 
 resource "azurerm_nat_gateway" "nat_gtw" {
@@ -27,16 +25,16 @@ resource "azurerm_nat_gateway" "nat_gtw" {
   tags = var.tags
 }
 
-resource "azurerm_nat_gateway_public_ip_prefix_association" "nat_ips" {
+resource "azurerm_nat_gateway_public_ip_association" "nat_gtw" {
 
   for_each = local.ips_with_index
 
-  nat_gateway_id      = azurerm_nat_gateway.nat_gtw.id
-  public_ip_prefix_id = azurerm_public_ip_prefix.nat_prefix[each.key].id
+  nat_gateway_id       = azurerm_nat_gateway.nat_gtw.id
+  public_ip_address_id = azurerm_public_ip.nat_gtw[each.key].id
 
 }
 
-resource "azurerm_subnet_nat_gateway_association" "sn_cluster_nat_gw" {
+resource "azurerm_subnet_nat_gateway_association" "nat_gtw" {
 
   for_each = toset(var.subnet_ids)
 

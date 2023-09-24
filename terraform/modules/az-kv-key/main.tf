e5b8ca998742d590e32bf3089f09a33d27e04aaf
Matthieu Nicolescu <mnicolescu@gmail.com>
2023-09-24 08:16:27 +0200

    feat: rename az-kv-cust-key to az-kv-key

--- terraform/modules/az-kv-key/main.tf
+++ terraform/modules/az-kv-key/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_key_vault_key" "key" {
   name         = var.name
   key_vault_id = var.kv_id
   key_type     = "RSA-HSM"
-  key_size     = 2048
+  key_size     = var.key_size
 
   key_opts = [
     "decrypt",
@@ -14,7 +14,7 @@ resource "azurerm_key_vault_key" "key" {
   ]
 
   dynamic "rotation_policy" {
-    for_each = var.rotation.expiration_date == null ? [] : ["rotation_policy"]
+    for_each = var.rotation.expire_after == null ? [] : ["rotation_policy"]
 
     content {
       automatic {
@@ -26,12 +26,16 @@ resource "azurerm_key_vault_key" "key" {
     }
   }
 
-  expiration_date = var.rotation.expiration_date
+  expiration_date = var.rotation.static_expiration_date
 
   lifecycle {
     precondition {
-      condition     = (var.rotation.expiration_date == null && var.rotation.expire_after != null) || (var.rotation.expiration_date != null && var.rotation.expire_after == null) || (var.rotation.expiration_date == null && var.rotation.expire_after == null)
-      error_message = "Either expiration_date or expire_after must be set"
+      condition     = (var.rotation.expire_after != null && var.rotation.auto_rotatation_time_before_expiry != null) || var.rotation.expire_after == null
+      error_message = "If rotation.expire_after is set, rotation.auto_rotatation_time_before_expiry must be set"
+    }
+    precondition {
+      condition     = (var.static_expiration_date != null && var.rotation.expire_after == null) || (var.static_expiration_date == null && var.rotation.expire_after != null) || (var.static_expiration_date == null && var.rotation.expire_after == null)
+      error_message = "If static_expiration_date is set, rotation.expire_after must be null"
     }
   }
 }
--- terraform/modules/az-kv-key/variables.tf
+++ terraform/modules/az-kv-key/variables.tf
@@ -1,20 +1,31 @@
 variable "name" {
-  description = "The name of the kv customer managed key"
+  description = "The name of the kv customer key"
   type        = string
 }
 
 variable "kv_id" {
-  description = "The ID of the Key Vault in which the Key Vault Key for the customer managed key be created."
+  description = "The ID of the Key Vault in which the Key Vault Key for the ctomer managed key be created."
   type        = string
 }
 
+variable "key_size" {
+  description = "The size of the key to create in the Key Vault."
+  type        = number
+  default     = 4096
+}
+
 variable "rotation" {
-  description = "The rotation policy of the customer managed key"
+  description = "The rotation policy of the managed key"
   type = object({
-    expiration_date                    = optional(string)
     auto_rotatation_time_before_expiry = optional(string, "P30D")
     expire_after                       = optional(string, "P90D")
     notify_before_expiry               = optional(string, "P29D")
   })
+}
+
+variable "static_expiration_date" {
+  description = "The static expiration date of the managed key"
+  type        = string
+  default     = null
 }
 

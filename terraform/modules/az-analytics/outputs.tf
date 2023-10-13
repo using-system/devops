945a28c46650d6cda616a4db4ea2a1a65f115e1c
Matthieu Nicolescu <mnicolescu@gmail.com>
2023-10-13 23:16:46 +0200

    Add test for az-analytics

--- terraform/modules/az-analytics/outputs.tf
+++ terraform/modules/az-analytics/outputs.tf
@@ -2,3 +2,20 @@ output "id" {
   description = "The Log Analytics Workspace ID"
   value       = azurerm_log_analytics_workspace.monitoring.id
 }
+
+output "primary_shared_key" {
+  description = "The Primary shared key for the Log Analytics Workspace"
+  value       = azurerm_log_analytics_workspace.monitoring.primary_shared_key
+  sensitive   = true
+}
+
+output "secondary_shared_key" {
+  description = "The Secondary  shared key for the Log Analytics Workspace"
+  value       = azurerm_log_analytics_workspace.monitoring.primary_shared_key
+  sensitive   = true
+}
+
+output "workspace_id" {
+  description = "The Workspace (or Customer) ID for the Log Analytics Workspace"
+  value       = azurerm_log_analytics_workspace.monitoring.workspace_id
+}
